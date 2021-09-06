2b8287db2d85072398cbfcc7af8b7a4577f842bc
PIAI20DEC4010 <christophermg2000.20@gmail.com>
2021-09-06 23:01:21 +0530

    third

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -7,8 +7,10 @@ import Typical from 'react-typical';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faGithub } from '@fortawesome/free-brands-svg-icons';
 import {  faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {  faInstagram } from '@fortawesome/free-brands-svg-icons';
 const git = <FontAwesomeIcon icon={faGithub} size="2x" />
 const li = <FontAwesomeIcon icon={faLinkedin} size="2x" />
+const insta = <FontAwesomeIcon icon={faInstagram} size="2x" />
 function Head(){
     return(
             
@@ -28,19 +30,21 @@ function Head(){
 
             </p>
             </div>
+
+                
             
-            
-                <div className="social inline space-between">
-                <a href="./" className="git-link">{git}</a>
-                <a href="./" className="git-link">{li}</a>
-                </div>
-                <div className="about inline">
+                <div className="about inline centre">
                     <div className="pro-pic"><img src={Data[0].photo} alt="" /></div>
                     <div className="writing">
                         <h1 className="main-title">M G Christopher</h1>
                         <h1>4th Year Btech CSE at IIIT Kottayam</h1>
                         {Data[0].about}
                     </div>    
+                    <div className="side">
+                    <div className="social-link"><a href="https://www.github.com/chistopher-2000" className="git-link">{git}</a></div>
+                    <div className="social-link"><a href="./" className="git-link">{li}</a></div>
+                    <div className="social-link"><a href="./" className="git-link">{insta}</a></div>
+                    </div>
                 </div>
                 
             </div>
