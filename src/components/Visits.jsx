da68722583dec90bee6716303aceecb7698418e2
Christopher <christopher.geever@gmail.com>
2023-12-05 17:23:58 -0500

    Fixed Visits

--- src/components/Visits.jsx
+++ src/components/Visits.jsx
@@ -1,47 +1,52 @@
-import React,{Component} from "react";
+import React, { useState, useEffect } from 'react';
 
-class Visits extends Component{
-    constructor(props) {
-        super(props);
-   
-        this.state = {
-            count: 0,
-            DataisLoaded: false
-        };
-    }
-    componentDidMount() {
-        fetch('https://api.api-ninjas.com/v1/counter?id=test_id&hit=true',{
+const Visits = () => {
+  // State to store the fetched data
+  const [count, setCount] = useState(1001);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Replace 'YOUR_API_KEY' with your actual API key
+        const apiKey = '9CLmvaQgg9cvn0r7GrLp6PzaJntnFN2TJwiEaw0l';
+
+        const response = await fetch(
+          'https://api.api-ninjas.com/v1/counter?id=test_id&hit=true',
+          {
             method: 'GET',
             headers: {
-              'X-Api-Key': 'Q3ckTswpedUDPwuNClGk4A==uQX72wMWwMWz4we2', // Replace 'YOUR_API_KEY' with your actual API key
-            }})
-    .then(res => res.json())
-    .then(res => {
-        this.setState({
-            count: res.value,
-            DataisLoaded: true
-        });
-        
-        
-    });
-    
-    }
-    render() {
-        const { DataisLoaded, count } = this.state;
-        if (!DataisLoaded) return <div>
-            <h1> Pleses wait some time.... </h1> </div> ;
-   
-        return (
-        <div className = "count center">
-            <br /><br /><br /><br /><br /><br />
-            <p style={{fontSize:20,margin:0}}>This page was viewed</p> 
-            <p style={{fontSize:40,margin:0}}>{count}</p>
-            <p style={{fontSize:20,margin:0}}>Times</p>
-            
-        </div>
-    );
-}
-}
+              'X-Api-Key': apiKey,
+              'Content-Type': 'application/json'
+            },
+          }
+        );
 
+        console.log(response);
+
+        const data = await response.json();
+        console.log(data);
+        // Update the state with the fetched count value
+        setCount(data.value);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Call the fetchData function when the component mounts
+    fetchData();
+
+
+    
+  }, []); // The empty dependency array ensures that the effect runs only once on mount
+
+  return (
+    <div className="count center">
+      <br /><br /><br /><br /><br /><br />
+      <p style={{ fontSize: 20, margin: 0 }}>This page was viewed</p>
+      <p style={{ fontSize: 40, margin: 0 }}>{count}</p>
+      <p style={{ fontSize: 20, margin: 0 }}>Times</p>
+    </div>
+  );
+};
 
 export default Visits;
